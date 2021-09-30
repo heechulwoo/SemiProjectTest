95c5a461c4554fb18cdd816ae68ae8b71374625d
sm408 <sm408@DESKTOP-6GM5SFR>
2021-09-30 14:55:46 +0900

    팀원 somin 클래스 내용이 변경됨

--- SemiProjectTest/src/teammember/somin/test/TeamMemberSomin.java
+++ SemiProjectTest/src/teammember/somin/test/TeamMemberSomin.java
@@ -5,6 +5,7 @@ public class TeamMemberSomin {
 	public static void main(String[] args) {
 		// 팀원 somin 이 작업한 것임.
 		System.out.println("연습용 : 팀원 somin 이 전용 브랜치에서 생성한 클래스");
+		System.out.println(">>> 클래스 내용이 변경됨 <<<");
 	}
 
 }
