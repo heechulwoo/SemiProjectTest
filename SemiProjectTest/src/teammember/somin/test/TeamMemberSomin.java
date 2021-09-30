380b9d1e51c1a6b35054312251c4554d620e06c9
sm408 <sm408@DESKTOP-6GM5SFR>
2021-09-30 14:54:40 +0900

    팀원 전용 브랜치인 leesm 브랜치에서 클래스 생성함

---
+++ SemiProjectTest/src/teammember/somin/test/TeamMemberSomin.java
@@ -0,0 +1,10 @@
+package teammember.somin.test;
+
+public class TeamMemberSomin {
+
+	public static void main(String[] args) {
+		// 팀원 somin 이 작업한 것임.
+		System.out.println("연습용 : 팀원 somin 이 전용 브랜치에서 생성한 클래스");
+	}
+
+}
