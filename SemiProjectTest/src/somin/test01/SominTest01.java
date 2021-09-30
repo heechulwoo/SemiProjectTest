38ec546e6b72a65bdf4e75d1c027f974689fb358
sm408 <sm408@DESKTOP-6GM5SFR>
2021-09-30 14:46:36 +0900

    팀원 somin 이 자바소스 파일 밎 HTML 파일 생성함.

---
+++ SemiProjectTest/src/somin/test01/SominTest01.java
@@ -0,0 +1,11 @@
+package somin.test01;
+
+public class SominTest01 {
+	
+	public static void main(String[] args) {
+		// 팀원 somin 이 작업한 것임.
+		System.out.println("연습용 : 팀원 somin 이 작업한 것임.");
+	}
+	
+	
+}
