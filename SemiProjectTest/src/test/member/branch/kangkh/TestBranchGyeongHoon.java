3e112fd1f805a0a25f75273f91a285cf6ab3e96d
user <user@DESKTOP-EP83E72>
2021-09-30 14:42:24 +0900

    kangkh branch에서 테스트 클래스 생성함.

---
+++ SemiProjectTest/src/test/member/branch/kangkh/TestBranchGyeongHoon.java
@@ -0,0 +1,9 @@
+package test.member.branch.kangkh;
+
+public class TestBranchGyeongHoon {
+
+	public static void main(String[] args) {
+		System.out.println("kangkh branch에서 테스트 클래스 생성함.");
+	}
+
+}
